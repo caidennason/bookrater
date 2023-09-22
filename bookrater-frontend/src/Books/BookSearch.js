2c1594934b7c0afd10bf645e85ace53b26f577e8
caiden nason <caidenbnason@gmail.com>
2023-09-22 02:00:31 -0400

    set up the form for book search

--- bookrater-frontend/src/Books/BookSearch.js
+++ bookrater-frontend/src/Books/BookSearch.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
 
 function BookSearch(){
 
+    const [formInput, setFormInput] = useState("")
+
+    const handleFormInput = (e) => {
+        setFormInput(e.target.value)
+    }
+
     return(
         <>
+        <Form>
+            <Form.Label>Search for a book</Form.Label>
+            <Form.Control
+            type="search" 
+            placeholder="Book title" 
+            value={formInput}
+            onChange={handleFormInput}/> 
+        </Form>
         </>
     )
 }
