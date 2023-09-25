d8e852721b1ca24803437cfd0845eb8c22d0a076
caiden nason <caidenbnason@gmail.com>
2023-09-24 21:09:04 -0400

    set up some error handling for the get request to open library api;

--- bookrater-frontend/src/Books/BookSearch.js
+++ bookrater-frontend/src/Books/BookSearch.js
@@ -6,6 +6,7 @@ function BookSearch(){
 
     const [title, setTitle] = useState('')
     const [author, setAuthor] = useState('')
+    const [books, setBooks] = useState(null)
 
     const handleTitleInput = (e) => {
         setTitle(e.target.value)
@@ -17,29 +18,47 @@ function BookSearch(){
 
     const findBook = () => {
         fetch(`https://openlibrary.org/search.json?title=${title}&author=${author}`)
-        .then((res) => res.json())
-        .then((data) => data)
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error('Something went wrong, try again')
+            }
+            return res.json()
+        })
+        .then((data) => {
+            return data;
+        })
+        .catch((error) => {
+            return { error: error.message }
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(title, author)
+        findBook()
+        setTitle('')
+        setAuthor('')
     }
 
     return(
         <div className="d-flex justify-content-center align-items-center vh-100">
-        <Form className="w-50 p-4 rounded bg-light rounded shadow">
+        <Form onSubmit={handleSubmit} className="w-50 p-4 rounded bg-light rounded shadow">
             <Form.Label>Search for a book</Form.Label>
             <Form.Control
             className="mb-3" 
-            controlId="name"
-            type="search" 
+            // controlId="name"
+            // // type="search" 
             placeholder="Book title" 
             value={title}
             onChange={handleTitleInput}/> 
             <Form.Control
             className="mb-3" 
-            controlId="name"
-            type="search" 
+            // controlId="name"
+            // // type="search" 
             placeholder="Book author" 
             value={author}
             onChange={handleAuthorInput}/> 
-            <Button>Search</Button>
+            <Button type="submit">Search</Button>
         </Form>
         </div>
     )
